8697d7fb3aacf4b566cf2045e7267eba72afe46c
Ferhat Cansev <fsunaycansev@gmail.com>
2025-04-16 00:27:03 +0300

    chore: add variables and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import "./App.css";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
+import styles from "./App.module.scss";
+
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <Suspense fallback={<>Loading...</>}>
         <Outlet />
       </Suspense>
-    </>
+    </div>
   );
 }
 
